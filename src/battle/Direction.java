f9192dfc31a68a6102321c6c9f3daad4a675a166
Matéo Castella <castella.mateo@gmail.com>
2021-02-19 15:43:17 +0100

    Initial commit

---
+++ src/battle/Direction.java
@@ -0,0 +1,14 @@
+package battle;
+
+/**
+ * @author Castella Matéo
+ * @version 1.0.0
+ * This enumeration list all direction possibilities
+ */
+
+public enum Direction {
+
+	HORIZONTAL,
+	VERTICAL;
+
+}
---
+++ src/battle/Mode.java
@@ -0,0 +1,15 @@
+package battle;
+
+/**
+ * @author Castella Matéo
+ * @version 1.0.0
+ * This enumeration list all mode possibilities
+ */
+
+public enum Mode {
+
+	HH,
+	HA,
+	AA;
+
+}
